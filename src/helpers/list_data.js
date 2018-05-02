eb30d9e3ba547663a7bc32fc948f81b1c66d52a5
samskarvan <samskarvan@gmail.com>
2018-05-02 16:23:56 -0700

    wrote functionality to display dummy data titles

---
+++ src/helpers/list_data.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        title: 'Aprender React',
+        details: 'Aprender como usar componentes'
+    },
+    {
+        title: 'Lavar la ropa',
+        details: 'Comprar detergente y prender el lavarropas'
+    },
+    {
+        title: 'Comprar Ultramarinos',
+        details: 'Necesitamos vegetales, aceite de oliva y pimiento negro'
+    },
+    {
+        title: 'Ir al Gimnasio',
+        details: 'Levantar pesas y hacer musculos'
+    }
+]
